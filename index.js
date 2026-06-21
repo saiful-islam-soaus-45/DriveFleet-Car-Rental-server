e345bb8042e302a9d4213d4763754af92b93e763
saiful-islam-soaus-45 <soausahmedbd91@gmail.com>
2026-06-21 13:44:20 +0600

    chore: remove node_modules and env from tracking

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv')
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 
 const uri = process.env.MONGODB_URI;
 const app = express()
 const PORT = process.env.PORT
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -17,14 +20,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("drivefleet");
+    const addCarCollection =  db.collection("add-Car");
+
+    app.post('/add-car', async (req, res) => {
+      const addCarData = req.body;
+      console.log(addCarData);
+      const result = await addCarCollection.insertOne(addCarData);
+      res.send(result);
+    })
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
